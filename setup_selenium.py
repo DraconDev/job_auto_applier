f5031584a39b9df94ec35f08c47dd2abc30277a3
dracondev <dracsharp@gmail.com>
2023-04-13 18:54:56 +0100

    update setup_selenium.cpython-311.pyc, main.py and setup_selenium.py

--- setup_selenium.py
+++ setup_selenium.py
@@ -1,5 +1,5 @@
+from selenium.common.exceptions import NoSuchElementException
 import json
-import math
 import os
 import random
 import re
@@ -11,7 +11,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 DOWNLOAD_FOLDER = "E:\\_DL\\"
+# monthly
 LINK = "https://www.linkedin.com/jobs/search/?currentJobId=3508072244&f_AL=true&f_TPR=r2592000&f_WT=2&geoId=101165590&keywords=full%20stack%20developer&location=United%20Kingdom&refresh=true"
+
+# Daily 50
+# LINK = "https://www.linkedin.com/jobs/search/?currentJobId=3556250799&f_AL=true&f_TPR=r86400&f_WT=2&geoId=101165590&keywords=full%20stack%20developer&location=United%20Kingdom&refresh=true&count=50&start=0"
 
 
 def setup_Chrome(folder=DOWNLOAD_FOLDER):
@@ -33,7 +37,7 @@ def setup_Chrome(folder=DOWNLOAD_FOLDER):
 
 
 def import_email():
-    with open("../../_login/_Site/indeed_login.json", "r") as f:
+    with open("../../_Login/_Site/indeed_login.json", "r") as f:
         return [*json.load(f).values()]
 
 
@@ -58,40 +62,63 @@ def open_site():
         By.CSS_SELECTOR, "button.btn__primary--large.from__button--floating"
     )
     login_button.click()
-    time.sleep(5)
+    time.sleep(10)
 
-    def click_next(driver, sleep=3):
+    def click_next(driver, sleep=2):
         time.sleep(random.randint(sleep, sleep * 2))
-        driver.find_elements(By.CSS_SELECTOR, "footer > div > button.ember-view")[
-            -1
-        ].click()
+        footers_buttons = driver.find_elements(
+            By.CSS_SELECTOR, "footer > div > button.ember-view"
+        )
+        if len(footers_buttons) > 0:
+            footers_buttons[-1].click()
 
     jobs = driver.find_elements(By.CSS_SELECTOR, "li.jobs-search-results__list-item")
 
-    for item in jobs[7:9]:
+    for item in jobs[29:48]:
         item.click()
         time.sleep(2)
-        driver.find_element(By.CSS_SELECTOR, "button.jobs-apply-button").click()
-        for i in range(4):
-            successful = driver.find_elements(
-                By.CSS_SELECTOR, "button.post-apply-add-skill__footer"
+
+        try:
+            apply = driver.find_element(By.CSS_SELECTOR, "button.jobs-apply-button")
+        except NoSuchElementException:
+            apply = False
+
+        if apply:
+            apply.click()
+            for i in range(4):
+                button_elements = driver.find_elements(By.XPATH, "//button/span")
+                successful = [
+                    button_element.text == "Done " for button_element in button_elements
+                ]
+                # successful = driver.find_elements(By.XPATH, "//button[@value='Done']")
+
+                if not successful:
+                    click_next(driver)
+                else:
+                    break
+
+                # if successful:
+                #     click_next(driver)
+                #     # successful.click()
+                #     time.sleep(3)
+                #     driver.find_element(By.TAG_NAME, "body").send_keys(Keys.ESCAPE)
+                #     break
+                # else:
+                #     click_next(driver)
+
+            close_window_button = driver.find_element(
+                By.CSS_SELECTOR, "button.artdeco-modal__dismiss"
             )
-            if successful:
-                successful.click()
-                break
-            else:
-                click_next(driver)
+            close_window_button.click()
 
-        close_window_button = driver.find_element(
-            By.CSS_SELECTOR, "button.artdeco-modal__dismiss"
-        )
-        close_window_button.click()
-
-        dismiss_button = driver.find_elements(
-            By.CSS_SELECTOR, "button.artdeco-modal__confirm-dialog-btn"
-        )[0]
-        dismiss_button.click()
-        time.sleep(2)
+            try:
+                dismiss_button = driver.find_elements(
+                    By.CSS_SELECTOR, "button.artdeco-modal__confirm-dialog-btn"
+                )
+            except NoSuchElementException:
+                dismiss_button = False
+            if dismiss_button:
+                dismiss_button[0].click()
 
     time.sleep(3000)
     return
